05cde1b5f1458421bbd3336427327b67712e297b
unknown <mossysm96@gmail.com>
2023-04-01 12:11:31 -0500

    finished functions... adding comments

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
-function d6(){
-    console.log("button clicked")
-    console.log("this is function d6")
-    numberMinMax(1,6)//calls numberMinMax function defined below
+function dice(rolls, elementid){
+    //console.log(rolls)
+    console.log(numberMinMax(1,rolls))//calls numberMinMax function defined below
+    document.getElementById(elementid).innerHTML=numberMinMax(1,rolls)
 }
 function d10(){
-    console.log("button clicked")
+    //console.log("button clicked")
     numberMinMax(1,11)
+    document.getElementById("roll10").innerHTML=numberMinMax(1,rolls)
 }
 function numberMinMax(min, max){ //creates numberMinMax function
-console.log(Math.floor(Math.random() * (max - min +1)) + min);  //defines parameters for numberMinMax function
+    const diceRoll=Math.floor(Math.random() * (max - min +1)) + min
+    //console.log(diceRoll)
+    return(diceRoll)
 }
